2045de39702cc7d2601c46546170804a2545537d
Benoit LeBlanc <benoit.leblanc@awakecoding.com>
2013-03-07 13:02:29 -0500

    Initial commit - ActiveX component test

---
+++ ActiveXClient/ActiveXClient/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ActiveXClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageActiveX = new System.Windows.Forms.TabPage();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.rdpConnection = new AxMSTSCLib.AxMsRdpClient8NotSafeForScripting();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonConnect = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDisconnect = new System.Windows.Forms.ToolStripButton();
+            this.tabPageUserDrawn = new System.Windows.Forms.TabPage();
+            this.toolStripContainer2 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonConnect2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDisconnect2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonRemoteProgram = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1.SuspendLayout();
+            this.tabPageActiveX.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpConnection)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.tabPageUserDrawn.SuspendLayout();
+            this.toolStripContainer2.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer2.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageActiveX);
+            this.tabControl1.Controls.Add(this.tabPageUserDrawn);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(796, 582);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPageActiveX
+            // 
+            this.tabPageActiveX.Controls.Add(this.toolStripContainer1);
+            this.tabPageActiveX.Location = new System.Drawing.Point(4, 22);
+            this.tabPageActiveX.Name = "tabPageActiveX";
+            this.tabPageActiveX.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageActiveX.Size = new System.Drawing.Size(788, 556);
+            this.tabPageActiveX.TabIndex = 0;
+            this.tabPageActiveX.Text = "ActiveX";
+            this.tabPageActiveX.UseVisualStyleBackColor = true;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.rdpConnection);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(782, 525);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(3, 3);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(782, 550);
+            this.toolStripContainer1.TabIndex = 2;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // rdpConnection
+            // 
+            this.rdpConnection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdpConnection.Enabled = true;
+            this.rdpConnection.Location = new System.Drawing.Point(0, 0);
+            this.rdpConnection.Name = "rdpConnection";
+            this.rdpConnection.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdpConnection.OcxState")));
+            this.rdpConnection.Size = new System.Drawing.Size(782, 525);
+            this.rdpConnection.TabIndex = 0;
+            this.rdpConnection.OnConnected += new System.EventHandler(this.rdpConnection_OnConnected);
+            this.rdpConnection.OnDisconnected += new AxMSTSCLib.IMsTscAxEvents_OnDisconnectedEventHandler(this.rdpConnection_OnDisconnected);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonConnect,
+            this.toolStripButtonDisconnect,
+            this.toolStripButtonRemoteProgram});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(287, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // toolStripButtonConnect
+            // 
+            this.toolStripButtonConnect.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonConnect.Image")));
+            this.toolStripButtonConnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonConnect.Name = "toolStripButtonConnect";
+            this.toolStripButtonConnect.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButtonConnect.Text = "Connect";
+            this.toolStripButtonConnect.Click += new System.EventHandler(this.toolStripButtonConnect_Click);
+            // 
+            // toolStripButtonDisconnect
+            // 
+            this.toolStripButtonDisconnect.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDisconnect.Image")));
+            this.toolStripButtonDisconnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDisconnect.Name = "toolStripButtonDisconnect";
+            this.toolStripButtonDisconnect.Size = new System.Drawing.Size(86, 22);
+            this.toolStripButtonDisconnect.Text = "Disconnect";
+            this.toolStripButtonDisconnect.Click += new System.EventHandler(this.toolStripButtonDisconnect_Click);
+            // 
+            // tabPageUserDrawn
+            // 
+            this.tabPageUserDrawn.Controls.Add(this.toolStripContainer2);
+            this.tabPageUserDrawn.Location = new System.Drawing.Point(4, 22);
+            this.tabPageUserDrawn.Name = "tabPageUserDrawn";
+            this.tabPageUserDrawn.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageUserDrawn.Size = new System.Drawing.Size(788, 556);
+            this.tabPageUserDrawn.TabIndex = 1;
+            this.tabPageUserDrawn.Text = "User Drawn";
+            this.tabPageUserDrawn.UseVisualStyleBackColor = true;
+            // 
+            // toolStripContainer2
+            // 
+            // 
+            // toolStripContainer2.ContentPanel
+            // 
+            this.toolStripContainer2.ContentPanel.Size = new System.Drawing.Size(782, 525);
+            this.toolStripContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer2.Location = new System.Drawing.Point(3, 3);
+            this.toolStripContainer2.Name = "toolStripContainer2";
+            this.toolStripContainer2.Size = new System.Drawing.Size(782, 550);
+            this.toolStripContainer2.TabIndex = 3;
+            this.toolStripContainer2.Text = "toolStripContainer2";
+            // 
+            // toolStripContainer2.TopToolStripPanel
+            // 
+            this.toolStripContainer2.TopToolStripPanel.Controls.Add(this.toolStrip2);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonConnect2,
+            this.toolStripButtonDisconnect2});
+            this.toolStrip2.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(170, 25);
+            this.toolStrip2.TabIndex = 0;
+            // 
+            // toolStripButtonConnect2
+            // 
+            this.toolStripButtonConnect2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonConnect2.Image")));
+            this.toolStripButtonConnect2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonConnect2.Name = "toolStripButtonConnect2";
+            this.toolStripButtonConnect2.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButtonConnect2.Text = "Connect";
+            this.toolStripButtonConnect2.Click += new System.EventHandler(this.toolStripButtonConnect2_Click);
+            // 
+            // toolStripButtonDisconnect2
+            // 
+            this.toolStripButtonDisconnect2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDisconnect2.Image")));
+            this.toolStripButtonDisconnect2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDisconnect2.Name = "toolStripButtonDisconnect2";
+            this.toolStripButtonDisconnect2.Size = new System.Drawing.Size(86, 22);
+            this.toolStripButtonDisconnect2.Text = "Disconnect";
+            this.toolStripButtonDisconnect2.Click += new System.EventHandler(this.toolStripButtonDisconnect2_Click);
+            // 
+            // toolStripButtonRemoteProgram
+            // 
+            this.toolStripButtonRemoteProgram.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonRemoteProgram.Image")));
+            this.toolStripButtonRemoteProgram.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonRemoteProgram.Name = "toolStripButtonRemoteProgram";
+            this.toolStripButtonRemoteProgram.Size = new System.Drawing.Size(117, 22);
+            this.toolStripButtonRemoteProgram.Text = "Remote Program";
+            this.toolStripButtonRemoteProgram.Click += new System.EventHandler(this.toolStripButtonRemoteProgram_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 582);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "RDP Control Test";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageActiveX.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpConnection)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabPageUserDrawn.ResumeLayout(false);
+            this.toolStripContainer2.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer2.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer2.ResumeLayout(false);
+            this.toolStripContainer2.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageActiveX;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private AxMSTSCLib.AxMsRdpClient8NotSafeForScripting rdpConnection;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonConnect;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDisconnect;
+        private System.Windows.Forms.TabPage tabPageUserDrawn;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer2;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonConnect2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDisconnect2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonRemoteProgram;
+
+    }
+}
+
---
+++ ActiveXClient/ActiveXClient/Form1.cs
@@ -0,0 +1,95 @@
+﻿using MSTSCLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActiveXClient
+{
+    public partial class Form1 : Form
+    {
+        private bool connected;
+
+        public bool Connected
+        {
+            get
+            {
+                return connected;
+            }
+            set
+            {
+                connected = value;
+                toolStripButtonRemoteProgram.Enabled = connected;
+                toolStripButtonConnect.Enabled = !connected;
+                toolStripButtonConnect2.Enabled = !connected;
+                toolStripButtonDisconnect.Enabled = connected;
+                toolStripButtonDisconnect2.Enabled = connected;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Connected = false;
+        }
+
+        private void toolStripButtonConnect_Click(object sender, EventArgs e)
+        {
+            rdpConnection.Server = "192.168.3.62";
+            rdpConnection.UserName = "Administrator";
+            rdpConnection.Domain = "Awake";
+
+            rdpConnection.DesktopWidth = rdpConnection.Width;
+            rdpConnection.DesktopHeight = rdpConnection.Height;
+
+            IMsTscNonScriptable secured = (IMsTscNonScriptable) rdpConnection.GetOcx();
+            secured.ClearTextPassword = "@wake01DC";
+
+            rdpConnection.Connect();
+        }
+
+        private void toolStripButtonDisconnect_Click(object sender, EventArgs e)
+        {
+            rdpConnection.Disconnect();
+        }
+
+        private void toolStripButtonDisconnect2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButtonConnect2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButtonRemoteProgram_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rdpConnection.RemoteProgram2.RemoteProgramMode = true;
+                rdpConnection.RemoteProgram2.ServerStartProgram(@"||cmd", "", "", false, "", false);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+
+        }
+
+        private void rdpConnection_OnConnected(object sender, EventArgs e)
+        {
+            Connected = true;
+        }
+
+        private void rdpConnection_OnDisconnected(object sender, AxMSTSCLib.IMsTscAxEvents_OnDisconnectedEvent e)
+        {
+            Connected = false;
+        }
+    }
+}
